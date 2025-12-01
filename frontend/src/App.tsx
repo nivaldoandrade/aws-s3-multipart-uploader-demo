bc066fdd4399f3f956b607a4caa214df6a01d1d5
nivaldoandrade <nivaldoandradef@gmail.com>
2025-12-01 13:47:15 -0300

    feat(frontend): initial ui setup with shadcn file input and button components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
 function App() {
   return (
-    <h1>Frontend</h1>
+    <div className='min-h-svh flex items-center justify-center'>
+      <div className='w-full max-w-2xl my-10 px-4'>
+        <h1 className='text-center sm:text-left text-4xl tracking-tighter'>
+          Selecione um arquivo:
+        </h1>
+        <form className='space-y-4 mt-5'>
+          <Input className='cursor-pointer' type='file' />
+          <Button className='w-full cursor-pointer' type='submit'>
+            Enviar
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
 
