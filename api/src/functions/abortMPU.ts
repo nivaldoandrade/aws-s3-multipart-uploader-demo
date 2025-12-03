ee66de7c5b8f2a5c8b3787f9e6adfebdef37be6b
nivaldoandrade <nivaldoandradef@gmail.com>
2025-12-03 12:11:55 -0300

    fix(api): add bucketName to request payload for endpoint

--- api/src/functions/abortMPU.ts
+++ api/src/functions/abortMPU.ts
@@ -2,11 +2,14 @@ import { AbortMultipartUploadCommand } from '@aws-sdk/client-s3';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import * as z from 'zod/mini';
 import { s3Client } from '../clients/s3Client';
-import { env } from '../config/env';
 import { bodyParser } from '../utils/bodyParser';
 import { lambdaHttpResponse } from '../utils/lambdaHttpResponse';
 
 const schema = z.object({
+  bucketName: z.string().check(
+    z.trim(),
+    z.minLength(1, '\'bucketName\' is required'),
+  ),
   key: z.string().check(
     z.trim(),
     z.minLength(1, '\'key\' is required'),
@@ -31,10 +34,10 @@ export async function handler(event: APIGatewayProxyEventV2) {
     });
   }
 
-  const { key, uploadId } = data;
+  const { bucketName, key, uploadId } = data;
 
   const command = new AbortMultipartUploadCommand({
-    Bucket: env.FILE_BUCKET_NAME,
+    Bucket: bucketName,
     Key: key,
     UploadId: uploadId,
   });
